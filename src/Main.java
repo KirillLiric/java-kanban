62b35b693f0ab5b474a91d92ee2a6164f4d3451d
Kir <Kirilla2012@gmail.com>
2024-12-17 23:13:12 +0300

    Спринт 7 V. 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-import managers.FileBackedTaskManager;
-import managers.Managers;
-import task.Task;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 
 public class Main {
 
